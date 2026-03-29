a323726e578d0c4d2653826284732a537614d9a4
kevinishimwe2 <k.ishimwe2@alustudent.com>
2026-03-29 15:08:45 +0200

    start of work

--- app/display.py
+++
@@ -1,6 +0,0 @@
-def display_diet_plan(plan: str) -> None:
-    print("\n" + "=" * 60)
-    print("         YOUR PERSONALISED DIET PLAN")
-    print("=" * 60)
-    print(plan)
-    print("=" * 60 + "\n")
